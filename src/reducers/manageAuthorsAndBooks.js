3fc3c91cff404d8af7d5ae5230b6ac7afefc86e6
Kevin Huang <yellow.khaos@gmail.com>
2019-02-09 18:19:14 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -7,28 +7,22 @@ const rootReducer = combineReducers({
 
 export default rootReducer
 
-export default function bookApp(state = {
-  authors: [],
-  books: []
-}, action) {
+function booksReducer (state = [], action) {
   let idx
   switch (action.type) {
 
     case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
+      return [...state, action.book]
 
     case "REMOVE_BOOK":
       idx = state.books.indexOf(action.id);
-      return {
-        ...state,
-        books: [
-          state.books.slice(0, idx),
-          state.books.slice(idx + 1),
-        ]
-      };
+      return [...state.slice(0, idx),...state.slice(idx + 1)]
+    default:
+      return state
+    
+    }
+}
+  
 
     case "ADD_AUTHOR":
         return {
